e4b9b62e406465ad4f68f1dece47e567df1fe951
Arnaldo Borda <aborda001@gmail.com>
2022-05-14 12:18:00 -0400

    Hooks for get all services

---
+++ src/hooks/useGetServices.js
@@ -0,0 +1,19 @@
+const useGetServices = () => {
+  const data = [
+    {
+      title: 'Web Development',
+      image: 'https://img.icons8.com/bubbles/100/000000/services.png',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deleniti maiores pariatur assumenda quas magni et, doloribus quod voluptate quasi molestiae magnam officiis dolorum, dolor provident atque molestias voluptatum explicabo!',
+    },
+    {
+      title: 'Mobile Development',
+      image: 'https://img.icons8.com/bubbles/100/000000/services.png',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deleniti maiores pariatur assumenda quas magni et, doloribus quod voluptate quasi molestiae magnam officiis dolorum, dolor provident atque molestias voluptatum explicabo!',
+    },
+  ];
+  return data;
+};
+
+export default useGetServices;
