27bf4722ba86f4143d61bd8289616dd025e73e19
Arnaldo Borda <aborda001@gmail.com>
2022-05-23 02:05:55 -0400

    Get projects hooks

---
+++ src/hooks/useGetProjects.js
@@ -0,0 +1,60 @@
+const useGetProjects = () => {
+  const data = [
+    {
+      title: 'Web Development',
+      image:
+        'https://images.unsplash.com/photo-1626968361222-291e74711449?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deleniti maiores pariatur assumenda quas magni et, doloribus quod voluptate quasi molestiae magnam officiis dolorum, dolor provident atque molestias voluptatum explicabo!',
+      code: 'www.github.com',
+      link: 'www.github.com',
+    },
+    {
+      title: 'Mobile Development',
+      image:
+        'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deleniti maiores pariatur assumenda quas magni et, doloribus quod voluptate quasi molestiae magnam officiis dolorum, dolor provident atque molestias voluptatum explicabo!',
+      code: 'www.github.com',
+      link: 'www.github.com',
+    },
+    {
+      title: 'Web Development',
+      image:
+        'https://images.unsplash.com/photo-1626968361222-291e74711449?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deleniti maiores pariatur assumenda quas magni et, doloribus quod voluptate quasi molestiae magnam officiis dolorum, dolor provident atque molestias voluptatum explicabo!',
+      code: 'www.github.com',
+      link: 'www.github.com',
+    },
+    {
+      title: 'Mobile Development',
+      image:
+        'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deleniti maiores pariatur assumenda quas magni et, doloribus quod voluptate quasi molestiae magnam officiis dolorum, dolor provident atque molestias voluptatum explicabo!',
+      code: 'www.github.com',
+      link: 'www.github.com',
+    },
+    {
+      title: 'Web Development',
+      image:
+        'https://images.unsplash.com/photo-1626968361222-291e74711449?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deleniti maiores pariatur assumenda quas magni et, doloribus quod voluptate quasi molestiae magnam officiis dolorum, dolor provident atque molestias voluptatum explicabo!',
+      code: 'www.github.com',
+      link: 'www.github.com',
+    },
+    {
+      title: 'Mobile Development',
+      image:
+        'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deleniti maiores pariatur assumenda quas magni et, doloribus quod voluptate quasi molestiae magnam officiis dolorum, dolor provident atque molestias voluptatum explicabo!',
+      code: 'www.github.com',
+      link: 'www.github.com',
+    },
+  ];
+  return data;
+};
+export default useGetProjects;
