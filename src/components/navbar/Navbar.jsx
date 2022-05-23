874c78894bb7fbc80f5237c2e990f60be6183b06
Arnaldo Borda <aborda001@gmail.com>
2022-05-23 01:59:39 -0400

    Editing ative-btn link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,57 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
-const Navbar = ({ isScrolling }) => {
-  const toTheTop = () => {
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-  };
+const Navbar = ({ activate }) => {
+  const active = activate.map((item) => {
+    return item ? "active-btn" : "";
+  });
 
   return (
-    <nav id="header">
-      <div className="header container">
-        <div className="nav-bar">
-          <div className="brand">
-            <a href="#hero" onClick={toTheTop}>
-              <h1>
-                <span>My</span> N<span>a</span>me
-              </h1>
-            </a>
-          </div>
-          <div className="nav-list">
-            <div className="hamburger">
-              <div className="bar"></div>
-            </div>
-            <ul>
-              <li>
-                <a href="#hero" data-after="Home">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#services" data-after="Service">
-                  Services
-                </a>
-              </li>
-              <li>
-                <a href="#projects" data-after="Projects">
-                  Projects
-                </a>
-              </li>
-              <li>
-                <a href="#about" data-after="About">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#contact" data-after="Contact">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <div className="controls">
+      <Link to="/" className={`control ${active[0]}`}>
+        <i className="fas fa-home"></i>
+      </Link>
+      <Link to="/about" className={`control ${active[1]}`}>
+        <i className="fas fa-user"></i>
+      </Link>
+      <Link to="/projects" className={`control ${active[2]}`}>
+        <i className="fas fa-briefcase"></i>
+      </Link>
+      <Link to="/blog" className={`control ${active[3]}`}>
+        <i className="far fa-newspaper"></i>
+      </Link>
+      <Link to="/contact" className={`control ${active[4]}`}>
+        <i className="fas fa-envelope-open"></i>
+      </Link>
+    </div>
   );
 };
 
