b5621ab8c7cec3e12f861d5bd018e1031ce948c9
Arnaldo Borda <aborda001@gmail.com>
2022-05-29 23:13:37 -0400

    Adding about page route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Project from "./pages/Project";
+import About from "./pages/About";
 import "./App.css";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Project />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
