822f53ea157e8ea726c4f10c0a0a7340c78cee64
Arnaldo Borda <aborda001@gmail.com>
2022-06-02 00:07:52 -0400

    Get skill hooks

---
+++ src/hooks/useGetSkills.js
@@ -0,0 +1,36 @@
+const useGetSkills = () => {
+  const data = [
+    {
+      url: 'https://i.ibb.co/Y7Dfr65/react.png',
+      alt: 'REACT',
+    },
+    {
+      url: 'https://i.ibb.co/mX6n2yq/web.png',
+      alt: 'WEB',
+    },
+    {
+      url: 'https://i.ibb.co/C12QnLr/git2.png',
+      alt: 'GIT',
+    },
+    {
+      url: 'https://i.ibb.co/2khNWd6/github.png',
+      alt: 'GITHUB',
+    },
+    {
+      url: 'https://i.ibb.co/s2ZH0P6/node-js.png',
+      alt: 'NODE',
+    },
+    {
+      url: 'https://i.ibb.co/8rS6vtn/postrges.png',
+      alt: 'POSTGRES',
+    },
+    {
+      url: 'https://i.ibb.co/wBTqw1y/pythonr.png',
+      alt: 'PYTHON',
+    },
+  ];
+
+  return data;
+};
+
+export default useGetSkills;
