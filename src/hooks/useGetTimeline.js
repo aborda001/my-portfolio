444bab710113cb21e55badd9801421e89a9be02c
Arnaldo Borda <aborda001@gmail.com>
2022-06-02 00:08:04 -0400

    get timeline hook

---
+++ src/hooks/useGetTimeline.js
@@ -0,0 +1,35 @@
+const useGetTimeline = () => {
+  const data = [
+    {
+      title: 'Web Development',
+      subTitle: 'VIRCROSOFT',
+      date: '2018 - Present',
+      description:
+        'I am currently working as a full-stack developer at VIRCROSOFT. I am responsible for developing and maintaining the front-end of the website, as well as the back-end. I am also responsible for creating and maintaining the database and the database schema.',
+    },
+    {
+      title: 'Web Development',
+      subTitle: 'VIRCROSOFT',
+      date: '2017 - 2018',
+      description:
+        'I was a full-stack developer at VIRCROSOFT. I was responsible for developing and maintaining the front-end of the website, as well as the back-end. I was also responsible for creating and maintaining the database and the database schema.',
+    },
+    {
+      title: 'Web Development',
+      subTitle: 'VIRCROSOFT',
+      date: '2016 - 2017',
+      description:
+        'I was a full-stack developer at VIRCROSOFT. I was responsible for developing and maintaining the front-end of the website, as well as the back-end. I was also responsible for creating and maintaining the database and the database schema.',
+    },
+    {
+      title: 'Web Development',
+      subTitle: 'VIRCROSOFT',
+      date: '2015 - 2016',
+      description:
+        'I was a full-stack developer at VIRCROSOFT. I was responsible for developing and maintaining the front-end of the website, as well as the back-end. I was also responsible for creating and maintaining the database and the database schema.',
+    },
+  ];
+  return data;
+};
+
+export default useGetTimeline;
