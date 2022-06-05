d8d7c9b5faa7fa00f73c197e64420a4d3aa1d171
Arnaldo Borda <aborda001@gmail.com>
2022-06-05 16:39:16 -0400

    Loading page component

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="div_contenedor">
+      <div className="div_centrado">
+        <div className="spinner"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
