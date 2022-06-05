28977352881f69fd36f1fd9d7cfd0d4f7706d766
Arnaldo Borda <aborda001@gmail.com>
2022-06-05 16:39:05 -0400

    Using loading page component

--- src/components/cover/Cover.jsx
+++ src/components/cover/Cover.jsx
@@ -1,27 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Loading from "../loading/Loading";
 import "./Cover.css";
 
 const Cover = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <div id="hero">
-      <div className="hero container">
-        <div>
-          <h1>
-            Hello, <span></span>
-          </h1>
-          <h1>
-            My name is <span></span>
-          </h1>
-          <h1>
-            Arnaldo Borda<span></span>
-          </h1>
-          <Link to="/projects" className="cta">
-            Portfolio
-          </Link>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div id="hero">
+          <div className="hero container">
+            <div>
+              <h1>
+                Hello, <span></span>
+              </h1>
+              <h1>
+                My name is <span></span>
+              </h1>
+              <h1>
+                Arnaldo Borda<span></span>
+              </h1>
+              <Link to="/projects" className="cta">
+                Portfolio
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
