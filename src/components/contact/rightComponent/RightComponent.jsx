c7d6173359962b92d8bd9bbf633ab2834d3c2911
Arnaldo Borda <aborda001@gmail.com>
2022-06-05 21:22:32 -0400

    Sending email

--- src/components/contact/rightComponent/RightComponent.jsx
+++ src/components/contact/rightComponent/RightComponent.jsx
@@ -1,35 +1,69 @@
-import React from "react";
-
+import React, { useRef } from "react";
+import { send } from "@emailjs/browser";
 import Download from "../../download/Download";
 
 const RightComponent = () => {
+  const form = useRef(null);
+  const { SERVICE_ID, TEMPLATE_ID, PUBLIC_KEY } = process.env;
+
+  const handleSubmit = () => {
+    const formData = new FormData(form.current);
+    const data = {
+      name: formData.get("name"),
+      email: formData.get("email"),
+      message: formData.get("message"),
+      subject: formData.get("subject"),
+    };
+    form.current.reset();
+
+    send(SERVICE_ID, TEMPLATE_ID, data, PUBLIC_KEY);
+  };
+
   return (
     <div className="right-contact">
-      <div className="contact-form">
+      <form className="contact-form" ref={form}>
         <div className="input-control i-c-2">
-          <input type="text" required placeholder="YOUR NAME" />
-          <input type="email" required placeholder="YOUR EMAIL" />
+          <input
+            type="text"
+            required
+            placeholder="YOUR NAME"
+            name="name"
+            id="name"
+          />
+          <input
+            type="email"
+            required
+            placeholder="YOUR EMAIL"
+            name="email"
+            id="email"
+          />
         </div>
         <div className="input-control">
-          <input type="text" required placeholder="ENTER SUBJECT" />
+          <input
+            type="text"
+            required
+            placeholder="ENTER SUBJECT"
+            name="subject"
+            id="subject"
+          />
         </div>
         <div className="input-control">
           <textarea
-            name=""
-            id=""
+            name="message"
+            id="message"
             cols="15"
             rows="8"
             placeholder="Message Here..."
           ></textarea>
         </div>
         <div className="submit-btn">
-          <button className="send-btn">
+          <button className="send-btn" onClick={handleSubmit} type="button">
             <span className="text">SUBMIT</span>
             <i className="fas fa-check icon"></i>
           </button>
           <Download />
         </div>
-      </div>
+      </form>
     </div>
   );
 };
