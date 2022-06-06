4d7fd3b9f3f92c214d54d56a4b140cba61ed298e
Arnaldo Borda <aborda001@gmail.com>
2022-06-06 02:10:50 -0400

    Adding CV PDF

--- src/components/download/Download.jsx
+++ src/components/download/Download.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 import "./Download.css";
+import pdf from "../../assets/files/Arnaldo_Borda_CV.pdf";
 
 const Download = () => {
   return (
     <div className="btn-con">
-      <a href="/" className="main-btn">
+      <a
+        href={pdf}
+        className="main-btn"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
         <span className="btn-text">Download CV</span>
         <span className="btn-icon">
           <i className="fas fa-download"></i>
