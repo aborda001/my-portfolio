cdb55beb73c472364978ce4b1067e1c5fa9a443c
Arnaldo Borda <aborda001@gmail.com>
2022-06-05 21:21:50 -0400

    Editign timeline info

--- src/hooks/useGetTimeline.js
+++ src/hooks/useGetTimeline.js
@@ -1,33 +1,35 @@
 const useGetTimeline = () => {
   const data = [
-    {
-      title: 'Web Development',
-      subTitle: 'VIRCROSOFT',
-      date: '2018 - Present',
-      description:
-        'I am currently working as a full-stack developer at VIRCROSOFT. I am responsible for developing and maintaining the front-end of the website, as well as the back-end. I am also responsible for creating and maintaining the database and the database schema.',
-    },
-    {
-      title: 'Web Development',
-      subTitle: 'VIRCROSOFT',
-      date: '2017 - 2018',
-      description:
-        'I was a full-stack developer at VIRCROSOFT. I was responsible for developing and maintaining the front-end of the website, as well as the back-end. I was also responsible for creating and maintaining the database and the database schema.',
-    },
-    {
-      title: 'Web Development',
-      subTitle: 'VIRCROSOFT',
-      date: '2016 - 2017',
-      description:
-        'I was a full-stack developer at VIRCROSOFT. I was responsible for developing and maintaining the front-end of the website, as well as the back-end. I was also responsible for creating and maintaining the database and the database schema.',
-    },
-    {
-      title: 'Web Development',
-      subTitle: 'VIRCROSOFT',
-      date: '2015 - 2016',
-      description:
-        'I was a full-stack developer at VIRCROSOFT. I was responsible for developing and maintaining the front-end of the website, as well as the back-end. I was also responsible for creating and maintaining the database and the database schema.',
-    },
+    [
+      {
+        title: 'Fullstack Developer',
+        subTitle: 'Sisteas Guaraní',
+        date: 'Sept 2021 - Present: 10 months',
+        description:
+          'Responsible for the development and maintenance of systems as well as implementation of new systems. I work developing APIs with NodeJS and ExpressJS, and with frontend technologies such as HTML, CSS, JavaScript. Use of relational databases (PostgreSQL). I also work with UNITY and C# for develop aumented reality applications.',
+      },
+      {
+        title: 'Frontend Web Developer',
+        subTitle: 'Software Natura',
+        date: 'Dec 2021 - Jun 2022: 7 months',
+        description:
+          'I worked as a Frontend Web Developer at Software Natura, contributing to educational web pages design and development using HTML, CSS. I created a Telegram bot to help my team and the other teams to extract and verify the id_tags nomenclature, this bot increased productivity and workflow. Worked with agile methodologies (Kanban). Using GIT and Bitbucket to manage the project, and AWS S3 to store the files and media.',
+      },
+    ],
+    [
+      {
+        title: 'Informatic Engineer',
+        subTitle: 'Catholic University',
+        date: 'March 2020 - Present',
+        description: '',
+      },
+      {
+        title: 'Informatic technician',
+        subTitle: 'SIL National School',
+        date: 'Feb 2017 - Dec 2019',
+        description: '',
+      },
+    ],
   ];
   return data;
 };
